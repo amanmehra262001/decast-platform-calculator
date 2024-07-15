8dc07efd6c99561b1fdcad7452f228713ce38da2
amanmehra262001 <amanmehra262001@gmail.com>
2024-07-15 12:07:22 +0530

    update: implement calculator with static prices

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,118 +1,152 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import { useEffect, useState } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
+type UserBalance = {
+  USDC: number;
+  USDT: number;
+  BZZ: number;
+  Minutes: number;
+};
+
+const initialBalance = {
+  USDC: 100,
+  USDT: 100,
+  BZZ: 100,
+  Minutes: 0,
+};
+
 export default function Home() {
+  const [currency, setCurrency] = useState("USDC");
+  const [userBalance, setUserBalance] = useState<UserBalance>(initialBalance);
+  const [rechargeAmount, setRechargeAmount] = useState(0);
+  const [BZZPrize, setBZZPrize] = useState(0.3216);
+  const [BZZPerGBPerMonth, setBZZPerGBPerMonth] = useState(0.3261);
+  const [minCastRecording, setMinCastRecording] = useState(1.2);
+  const [minutesAllocated, setMinutesAllocated] = useState(0);
+
+  const handleChange = (e: any) => {
+    switch (e.target.name) {
+      case "amount":
+        setRechargeAmount(e.target.value);
+        // getMinutesAllocated();
+        break;
+      case "currency":
+        setCurrency(e.target.value);
+        break;
+      default:
+        break;
+    }
+  };
+
+  const getMinutesAllocated = () => {
+    let bzzAmount = rechargeAmount;
+    if (currency !== "BZZ") {
+      bzzAmount = rechargeAmount / BZZPrize;
+    }
+    const mbperBZZperMonth = 1024 / BZZPerGBPerMonth;
+    const minutesAllocated = (bzzAmount * mbperBZZperMonth) / minCastRecording;
+    setMinutesAllocated(parseFloat(minutesAllocated.toFixed(2)));
+  };
+
+  const handleClickRecharge = () => {
+    setUserBalance({
+      ...userBalance,
+      [currency]: userBalance[currency as keyof UserBalance] - rechargeAmount,
+      Minutes: userBalance.Minutes + minutesAllocated,
+    });
+    setRechargeAmount(0);
+  };
+
+  useEffect(() => {
+    getMinutesAllocated();
+  }, [rechargeAmount, currency]);
+
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      className={`flex min-h-screen flex-col items-center p-4 md:p-24 ${inter.className}`}
     >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+      <header className="w-full md:w-1/2 flex items-center justify-between px-2">
+        <p>User Balance:</p>
+        <p>{userBalance.USDC.toFixed(2)} USDC</p>
+        <p>{userBalance.USDT.toFixed(2)} USDT</p>
+        <p>{userBalance.BZZ.toFixed(2)} BZZ</p>
+        <p>{userBalance.Minutes.toFixed(2)} Minutes</p>
+      </header>
+      <section className="w-full md:w-1/2 border border-gray-700 rounded-lg p-4 flex flex-col gap-4">
+        <div>
+          <div className="flex gap-2">
+            <input
+              type="number"
+              className="w-4/5 p-2 bg-transparent rounded-lg border border-gray-700"
+              placeholder="Enter amount"
+              name="amount"
+              value={rechargeAmount}
+              onChange={handleChange}
             />
-          </a>
+            <select
+              className="w-1/5 text-center border border-gray-700 rounded-lg"
+              name="currency"
+              id="currency-select"
+              value={currency}
+              onChange={handleChange}
+            >
+              <option value="USDC">USDC</option>
+              <option value="USDT">USDT</option>
+              <option value="BZZ">BZZ</option>
+            </select>
+          </div>
+          <div className="w-full text-gray-500 text-xs text-right px-1">
+            {currency !== "BZZ" ? (
+              <p>
+                {rechargeAmount} {currency} ≈{" "}
+                {currency !== "BZZ"
+                  ? (rechargeAmount / BZZPrize).toFixed(2)
+                  : 1}{" "}
+                BZZ
+              </p>
+            ) : (
+              <p>{rechargeAmount} BZZ</p>
+            )}
+          </div>
         </div>
-      </div>
+        <div className="flex gap-2">
+          <input
+            type="number"
+            className="w-4/5 p-2 bg-transparent rounded-lg border border-gray-700"
+            placeholder="Minutes Redeemable"
+            value={minutesAllocated}
+            disabled
+          />
+          <div className="w-1/5 flex items-center justify-center border border-gray-700 rounded-lg">
+            ~Minutes
+          </div>
+        </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+        <div className="flex flex-col gap-4 text-gray-500 text-xs px-2">
+          <div className="flex items-center justify-between">
+            <p>BZZ Prize(USD)</p>
+            <p>{BZZPrize}</p>
+          </div>
+          <div className="flex items-center justify-between">
+            <p>BZZ per GB per month</p>
+            <p>{BZZPerGBPerMonth}</p>
+          </div>
+          <div className="flex items-center justify-between">
+            <p>1 min cast recording(AVG)</p>
+            <p>{minCastRecording} MB</p>
+          </div>
+        </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+        <button
+          className="w-full p-2 bg-gray-400 hover:bg-gray-200 text-black rounded-lg"
+          onClick={handleClickRecharge}
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+          Recharge
+        </button>
+      </section>
     </main>
   );
 }
